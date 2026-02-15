ffb40d121c0b587780b1dc8b6ce33496d6ed4611
unknown <hridays2005@gmail.com>
2026-02-15 23:21:34 +0530

    fixed pdf generation errors

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      "@sparticuz/chromium",
+      "puppeteer-core",
+    ],
+  },
   allowedDevOrigins: ["http://192.168.180.162:3000"],
   images: {
     remotePatterns: [
